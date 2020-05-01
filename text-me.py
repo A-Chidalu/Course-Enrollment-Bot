ed1c262257e5fdd1520bccad1a14a67a05b189a0
Chidalu Agbakwa <cagbakwa@gmail.com>
2020-04-30 22:04:35 -0400

    Changes

--- text-me.py
+++ text-me.py
@@ -5,9 +5,11 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC05c0692e167c2f177d35ebb0a2662f84'
-auth_token = 'your_auth_token'
+
+
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+
 
 message = client.messages \
     .create(
